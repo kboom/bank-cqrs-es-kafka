edaa5079fbc736b55e13d5ccb5a9b3c6d9006593
kbhit <gurgul.grzegorz@gmail.com>
2018-05-28 21:49:36 +0200

    Removes unnecessary config files. No running locally.

--- application/bank-account-management/scripts/run.dev.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar target/bank-account-management-1.0-SNAPSHOT.jar server ../environments/development.yml
--- application/bank-customer-management/scripts/run.dev.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-java -jar target/bank-customer-management-1.0-SNAPSHOT.jar server ../environments/development.yml
